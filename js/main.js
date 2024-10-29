e70a2c15356aabeb4f85fb36b2941975f542ec98
ImuneGH <MartinSotnar93@gmail.com>
2024-10-29 11:18:55 +0100

    vylepsil jsem fixovani navbaru, ale porad to neni uplne plynule

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-console.log("ahoj");
-
 window.onscroll = function() {
-    let trackedElement = document.getElementById("desktopNav");
+    let trackedElement = document.getElementById("positionPoint");
     let rect = trackedElement.getBoundingClientRect(); // Získáme pozici elementu vzhledem k viewportu
     let header = document.getElementById("MS");
+    let navBar = document.getElementById("desktopNav");
 
     // Kontrola, zda je element v horní části okna (nebo pod určitým bodem)
-    if (rect.top <= 0) {
-        trackedElement.classList.add("fixed"); // Přidání třídy pro fixování
+    if (rect.top <= 60) {
+        navBar.classList.add("fixed"); // Přidání třídy pro fixování
         header.classList.add("navMargin"); // přidání marginu, který vyplní místo navbaru
-    } else {
-        trackedElement.classList.remove("fixed"); // Odebrání třídy při skrolování zpět nahoru
+    } else if(rect.top > 60){
+        navBar.classList.remove("fixed"); // Odebrání třídy při skrolování zpět nahoru
+        header.classList.remove("navMargin"); // Odebrání třídy při skrolování zpět nahoru
     }
 
     // Pro ukázku můžeme do konzole vypisovat pozici elementu
