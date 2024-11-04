d81174a1ab157967d614d9f064e8a0f3802a06bd
ImuneGH <MartinSotnar93@gmail.com>
2024-11-04 15:06:55 +0100

    pridal jsem animaci, ale pozdeji pohledam nejakou lepsi

--- js/main.js
+++ js/main.js
@@ -8,9 +8,12 @@ window.onscroll = function() {
     if (rect.top <= 60) {
         navBar.classList.add("fixed"); // Přidání třídy pro fixování
         header.classList.add("navMargin"); // přidání marginu, který vyplní místo navbaru
+        navBar.classList.add("animate__animated", "animate__pulse");
     } else if(rect.top > 60){
         navBar.classList.remove("fixed"); // Odebrání třídy při skrolování zpět nahoru
         header.classList.remove("navMargin"); // Odebrání třídy při skrolování zpět nahoru
+        navBar.classList.remove("animate__animated", "animate__pulse");
+        // navBar.classList.add("animate__animated", "animate__flipOutX");
     }
 
     // Pro ukázku můžeme do konzole vypisovat pozici elementu
