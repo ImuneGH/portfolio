1ba2ab74fe83c2f7e7b424a247a1254892a75c42
ImuneGH <MartinSotnar93@gmail.com>
2024-11-18 09:58:35 +0100

    pridal jsem animaci navbaru s kterou jsem konecne docela spokojen - main.js

--- js/main.js
+++ js/main.js
@@ -11,14 +11,12 @@ addEventListener("scroll", () =>
     if (rect.top <= 60) {
         navBar.classList.add("fixed"); // Přidání třídy pro fixování
         header.classList.add("navMargin"); // přidání marginu, který vyplní místo navbaru
-        navBar.classList.add("animate__animated", "animate__pulse");
+        navBar.classList.add("animate__animated", "animate__fadeInDown", "animate__fast");
     } else if(rect.top > 60){
         navBar.classList.remove("fixed"); // Odebrání třídy při skrolování zpět nahoru
         header.classList.remove("navMargin"); // Odebrání třídy při skrolování zpět nahoru
-        navBar.classList.remove("animate__animated", "animate__pulse");
-        // navBar.classList.add("animate__animated", "animate__flipOutX");
+        navBar.classList.remove("animate__animated", "animate__fadeInDown", "animate__fast");
     }
-    
 
     // Pro ukázku můžeme do konzole vypisovat pozici elementu
     console.log("Top:", rect.top, "Bottom:", rect.bottom);
