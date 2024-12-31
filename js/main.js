60332eafafc417b90c0ab55f5cb82fa5faeb6c4c
ImuneGH <MartinSotnar93@gmail.com>
2024-12-31 12:52:00 +0100

    fixnuta animace na obrazkach po pridani odkazu <a>

--- js/main.js
+++ js/main.js
@@ -286,12 +286,18 @@ const projectImages = document.querySelectorAll(".hoverScale");
 
 projectImages.forEach(projectImg =>    {
     projectImg.addEventListener("mouseenter", event => {
-        gsap.to(event.target, {scale: 1.2, duration: .5});
-        gsap.to(event.target.nextElementSibling, {scale: 1.2, duration: .5});
+        const target = event.currentTarget;
+        const parent = target.closest(".projectPic");
+        const shadowImg = parent.querySelector(".shadow");
+        gsap.to(target, {scale: 1.2, duration: .5});
+        gsap.to(shadowImg, {scale: 1.2, duration: .5});
     });
     projectImg.addEventListener("mouseout", event => {
-        gsap.to(event.target, {overwrite: true, scale: 1, duration: .3});
-        gsap.to(event.target.nextElementSibling, {overwrite: true, scale: 1, duration: .3});
+        const target = event.currentTarget;
+        const parent = target.closest(".projectPic");
+        const shadowImg = parent.querySelector(".shadow");
+        gsap.to(target, {overwrite: true, scale: 1, duration: .3});
+        gsap.to(shadowImg, {overwrite: true, scale: 1, duration: .3});
     });
 });
 
