57e9338c6264a8e8165bb39c8ee27dfe33b135ee
ImuneGH <MartinSotnar93@gmail.com>
2025-08-08 19:42:35 +0200

    už to funguje, přemýšlím jak to ještě vylepšít, ale atm jsem docela spoko a už trochu spadl kámen ze srdce ^^

--- js/main.js
+++ js/main.js
@@ -32,20 +32,16 @@ async function langFetch(langChoice) {
 function scaleAnimationEnter(event) {
   const target = event.currentTarget;
   const parent = target.parentElement;
-  const sibling = target.nextElementSibling
-    ? target.nextElementSibling
-    : parent.nextElementSibling;
-  gsap.to(target, { scale: 1.2, duration: 0.5 });
+  const sibling = parent.nextElementSibling;
+  gsap.to(parent, { scale: 1.2, duration: 0.5 });
   gsap.to(sibling, { scale: 1.2, duration: 0.5 });
 }
 
 function scaleAnimationLeave(event) {
   const target = event.currentTarget;
   const parent = target.parentElement;
-  const sibling = target.nextElementSibling
-    ? target.nextElementSibling
-    : parent.nextElementSibling;
-  gsap.to(target, { overwrite: true, scale: 1, duration: 0.3 });
+  const sibling = parent.nextElementSibling;
+  gsap.to(parent, { overwrite: true, scale: 1, duration: 0.3 });
   gsap.to(sibling, { overwrite: true, scale: 1, duration: 0.3 });
 }
 
