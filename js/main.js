2216b65f38f3868bbae2240d36f32a0f0ea5633d
ImuneGH <MartinSotnar93@gmail.com>
2026-04-04 16:48:03 +0200

    fix: opraven bug se špatným switchem mezi dark/light modem při nastavení systému na dark

--- js/main.js
+++ js/main.js
@@ -723,51 +723,42 @@ handleLinks();
 
 // light/dark mode toggle
 
-let defaultColorTheme = null;
-let storedColorTheme = localStorage.getItem("colorTheme");
-let systemColorIsDark = window.matchMedia(
-  "(prefers-color-scheme: dark)",
-).matches;
-let actualTime = new Date().getHours();
-const colorThemeSwitches = document.querySelectorAll(".checkbox");
-const bodyElement = document.body;
-let bodyIsDark = null;
-
-if (storedColorTheme) {
-  defaultColorTheme = storedColorTheme;
-} else if (systemColorIsDark) {
-  defaultColorTheme = "dark";
-} else if (actualTime >= 6 && actualTime < 22) {
-  defaultColorTheme = "light";
-} else {
-  defaultColorTheme = "dark";
-}
-
-if (defaultColorTheme === "dark") {
-  bodyElement.classList.add("dark");
-  colorThemeSwitches.forEach((colorSwitch) => {
-    colorSwitch.checked = true;
+function themeSwitch(checkboxes, isChecked) {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = isChecked;
   });
 }
 
-bodyIsDark = bodyElement.className.includes("dark");
+const storedColorTheme = localStorage.getItem("colorTheme");
+const systemColorIsDark = window.matchMedia(
+  "(prefers-color-scheme: dark)",
+).matches;
+const actualTime = new Date().getHours();
+const colorThemeSwitches = document.querySelectorAll(".checkbox");
+const bodyElement = document.body;
+
+if (storedColorTheme) {
+  storedColorTheme === "dark" && bodyElement.classList.add("dark");
+} else if (systemColorIsDark) {
+  bodyElement.classList.add("dark");
+} else if (actualTime <= 6 || actualTime > 22) {
+  bodyElement.classList.add("dark");
+}
+
+if (bodyElement.classList.contains("dark")) {
+  themeSwitch(colorThemeSwitches, true);
+}
 
 colorThemeSwitches.forEach((colorSwitch) => {
   colorSwitch.addEventListener("change", () => {
-    if (bodyIsDark) {
+    if (bodyElement.classList.contains("dark")) {
       localStorage.setItem("colorTheme", "light");
       bodyElement.classList.remove("dark");
-      colorThemeSwitches.forEach((colorSwitch) => {
-        colorSwitch.checked = false;
-      });
-      bodyIsDark = false;
+      themeSwitch(colorThemeSwitches, false);
     } else {
       localStorage.setItem("colorTheme", "dark");
       bodyElement.classList.add("dark");
-      colorThemeSwitches.forEach((colorSwitch) => {
-        colorSwitch.checked = true;
-      });
-      bodyIsDark = true;
+      themeSwitch(colorThemeSwitches, true);
     }
   });
 });
