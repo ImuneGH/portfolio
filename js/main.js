5ce174508f614b81cbd4dee59ba63cbe800c52f5
ImuneGH <MartinSotnar93@gmail.com>
2026-04-05 17:19:12 +0200

    refactor: přidán zapomenutý import do navbar.js, refaktor dokončen a otestován

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-import { navbar } from "./navbar";
 import { theme } from "./theme";
 import { languages } from "./languages";
 import { animations } from "./animations";
 import { game } from "./game";
+import { navbar } from "./navbar";
 
-navbar();
 theme();
 languages();
 animations();
 game();
+navbar();
